61e4983326fb9292e9583264405099beb6918b85
abdou <soniaakrout81@gmail.com>
2026-04-09 14:57:05 +0100

    Backend

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,14 @@
 ﻿import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import axios from "axios";
 import "./index.css";
 import "./i18n";
 import App from "./App.jsx";
 import { store } from "./app/store";
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "";
+axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
